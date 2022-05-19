30be8ad3f61571af8ab34668ac993a2c184adba5
Laura Villarroel <lauramar.villarroel@gmail.com>
2022-05-19 13:01:19 -0500

    Update db.js

--- Api/src/db.js
+++ Api/src/db.js
@@ -61,9 +61,9 @@ const { Medic, Pacient, Calendar, Hc, User, Turn, Budget } = sequelize.models;
 Medic.belongsToMany(Pacient, { through: Calendar });
 Pacient.hasOne(Medic, { through: Calendar });
 
-// relacion usario- paciente (1:1)
-/* User.hasOne(Pacient);
-Pacient.belongsTo(User); */
+//|!|  relacion usario- paciente (1:1)
+//|| User.hasOne(Pacient);
+//|| Pacient.belongsTo(User);
 // relacion paciente-presupuesto (1:N)
 Pacient.hasMany(Budget);
 Budget.belongsTo(Pacient);
