49856cf256fd85575789e863e45fa0d1e84b1da4
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-05-21 09:40:29 -0300

    Modelo Budget => Limpio

--- Api/src/models/Budget.js
+++ Api/src/models/Budget.js
@@ -1,32 +1,26 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = sequelize => {
-  // defino el modelo
-  sequelize.define(
-    'Budget',
-    {
-      ID: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-      },
-      date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      services: {
-        type: DataTypes.JSON,
-        allowNull: false,
-      },
-      discount: {
-        type: DataTypes.FLOAT,
-        defaultValue: null,
-      },
-      totalPrice: {
-        type: DataTypes.FLOAT,
-        allowNull: false,
-      },
+  sequelize.define('Budget', {
+    ID: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
     },
-    { timestamps: false } //eliminar
-  );
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    services: {
+      type: DataTypes.JSON,
+      allowNull: false,
+    },
+    discount: {
+      type: DataTypes.FLOAT,
+      defaultValue: null,
+    },
+    totalPrice: {
+      type: DataTypes.FLOAT,
+      allowNull: false,
+    },
+  });
 };
