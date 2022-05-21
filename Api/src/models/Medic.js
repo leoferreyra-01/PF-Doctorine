b7a1fea7365a8d5df77a604c8586b4ff2a5fa4e2
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-05-21 10:17:28 -0300

    Model Medic => Limpio

--- Api/src/models/Medic.js
+++ Api/src/models/Medic.js
@@ -1,27 +1,24 @@
 const { DataTypes } = require('sequelize');
+
 module.exports = sequelize => {
-  sequelize.define(
-    'Medic',
-    {
-      ID: {
-        type: DataTypes.UUID,
-        defaultValue: DataTypes.UUIDV4,
-        primaryKey: true,
-      },
-      title: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
-      specialization: {
-        type: DataTypes.STRING,
-      },
-      tuition_date: {
-        type: DataTypes.DATE,
-      },
-      tuition_number: {
-        type: DataTypes.INTEGER,
-      },
+  sequelize.define('Medic', {
+    ID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    { timestamps: false }
-  );
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    specialization: {
+      type: DataTypes.STRING,
+    },
+    tuition_date: {
+      type: DataTypes.DATE,
+    },
+    tuition_number: {
+      type: DataTypes.INTEGER,
+    },
+  });
 };
