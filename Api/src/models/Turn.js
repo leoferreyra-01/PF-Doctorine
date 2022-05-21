b788dfbdddc5513d619c3003d4a0cce3b4d82772
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-05-21 10:30:34 -0300

    Model Turn => Limpio

--- Api/src/models/Turn.js
+++ Api/src/models/Turn.js
@@ -1,28 +1,18 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
+
 module.exports = sequelize => {
-  // defino el modelo
-  sequelize.define(
-    'Turn',
-    {
-      ID: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-      },
-      date: {
-        type: DataTypes.DATE,
-        allowNull: false,
-      },
-      name: {
-        // no  creo que haga falta
-        type: DataTypes.STRING,
-      },
-      description: {
-        // no  creo que haga falta
-        type: DataTypes.STRING,
-      },
+  sequelize.define('Turn', {
+    ID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    { timestamps: false } //eliminar
-  );
+    date: {
+      type: DataTypes.DATE,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.TEXT,
+    },
+  });
 };
