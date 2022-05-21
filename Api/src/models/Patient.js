11bb1864476a6878cbaf049760a0ad75cfc90809
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-05-21 10:38:38 -0300

    Model Pacient a Patient

--- Api/src/models/Patient.js
+++ Api/src/models/Patient.js
@@ -1,7 +1,7 @@
 const { DataTypes } = require('sequelize');
 
 module.exports = sequelize => {
-  sequelize.define('Pacient', {
+  sequelize.define('Patient', {
     ID: {
       type: DataTypes.INTEGER,
       autoIncrement: true,
