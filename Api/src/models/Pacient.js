37a70d99b30a072cb8997293a43a07e7323b50c6
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-05-21 10:19:03 -0300

    Model Pacient => Limpio

--- Api/src/models/Pacient.js
+++ Api/src/models/Pacient.js
@@ -1,25 +1,20 @@
 const { DataTypes } = require('sequelize');
-// Exportamos una funcion que define el modelo
-// Luego le injectamos la conexion a sequelize.
-module.exports = sequelize => {
-  // defino el modelo
-  sequelize.define(
-    'Pacient',
-    {
-      ID: {
-        type: DataTypes.INTEGER,
-        primaryKey: true,
-      },
-      medicalService: {
-        type: DataTypes.STRING,
-        defaultValue: null,
-      },
 
-      name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-      },
+module.exports = sequelize => {
+  sequelize.define('Pacient', {
+    ID: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
     },
-    { timestamps: false } //eliminar
-  );
+    medicalService: {
+      type: DataTypes.STRING,
+      defaultValue: null,
+    },
+
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
 };
