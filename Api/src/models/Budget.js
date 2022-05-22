f8b2f13c84bdb7867af44b82c2e4ffb10b059206
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-05-22 15:40:15 -0300

    Cambia servi por treatments

--- Api/src/models/Budget.js
+++ Api/src/models/Budget.js
@@ -11,7 +11,7 @@ module.exports = sequelize => {
       type: DataTypes.DATE,
       allowNull: false,
     },
-    services: {
+    treatments: {
       type: DataTypes.JSON,
       allowNull: false,
     },
