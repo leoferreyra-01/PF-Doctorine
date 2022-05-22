135ad5fa856c8cdf77acfb4c91e1ede88727618a
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-05-22 16:27:11 -0300

    defaultValue: false, para todos!

--- Api/src/models/ClinicalHistory.js
+++ Api/src/models/ClinicalHistory.js
@@ -14,21 +14,27 @@ module.exports = sequelize => {
     },
     b_useDrugs: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     b_pregnant: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     b_takeMedicine: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     b_sufferedFromIllness: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     b_alergicToMedicine: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     b_normalWoundHealing: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     b_other: {
       type: DataTypes.STRING,
@@ -37,15 +43,19 @@ module.exports = sequelize => {
     // INFECTION, CARDIOVASCULAR
     ic_anginaPectoris: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ic_myocardialInfarction: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ic_hypertension: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ic_vascularAffections: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ic_other: {
       type: DataTypes.STRING,
@@ -54,15 +64,19 @@ module.exports = sequelize => {
     // INFECTION, HEMATOLOGICAL
     ih_anemia: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ih_leukemia: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ih_haemophilia: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ih_alterationWhiteSerie: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ih_other: {
       type: DataTypes.STRING,
@@ -71,18 +85,23 @@ module.exports = sequelize => {
     // INFECTION, RESPIRATORY
     ir_asthma: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ir_pulmonaryEdema: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ir_ephysemia: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ir_tuberculosis: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ir_chronicBronchitis: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ir_other: {
       type: DataTypes.STRING,
@@ -91,12 +110,15 @@ module.exports = sequelize => {
     // INFECTION, GASTROINTESTINAL
     ig_ulcer: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ig_hepatitis: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ig_cirrohsis: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ig_other: {
       type: DataTypes.STRING,
@@ -105,12 +127,15 @@ module.exports = sequelize => {
     // INFECTION, NERVOUS
     in_epilepsy: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     in_useOfTranquilizers: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     in_seizures: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     in_other: {
       type: DataTypes.STRING,
@@ -119,15 +144,19 @@ module.exports = sequelize => {
     // INFECTION, BONES
     ib_osteoporosis: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ib_paget: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ib_rickets: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ib_osteomalacia: {
       type: DataTypes.BOOLEAN,
+      defaultValue: false,
     },
     ib_other: {
       type: DataTypes.STRING,
