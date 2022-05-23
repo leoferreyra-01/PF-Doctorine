5b2bb857547bedfbe04d0d51192661fa5d457c8e
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-05-22 21:31:23 -0300

    Import controllers a index

--- Api/src/routes/index.js
+++ Api/src/routes/index.js
@@ -8,24 +8,28 @@ const router = require('express').Router();
 //|> RUTES
 
 //#region GET
+const {} = require('./controllersGET');
 
 router.get('/', async (req, res) => {});
 
 //#endregion
 
 //#region POST
+const {} = require('./controllersPOST');
 
 router.post('/', async (req, res) => {});
 
 //#endregion
 
 //#region PUT
+const {} = require('./controllersPUT');
 
 router.put('/', async (req, res) => {});
 
 //#endregion
 
 //#region DELETE
+const {} = require('./controllersDELETE');
 
 router.delete('/', async (req, res) => {});
 
