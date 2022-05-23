fbc3adf294fd030581c541c74030d16a8913842b
Joseph Palacios <50757691+SilverSystem@users.noreply.github.com>
2022-05-23 05:45:24 -0400

    Solucion de los forms con el ruteo de Debo

--- client/src/App.js
+++ client/src/App.js
@@ -3,19 +3,18 @@ import React from 'react';
 import { Route } from 'react-router-dom';
 import LandingPage from './landingPage';
 import Home from './dentist/home';
-import CreatePatient from './dentist/home/navbar/create';
-
+import RegisterPatient from './dentist/home/navbar/register patient/';
 
 function App() {
   return (
     <div className="App">
-      <Route exact path='/' render={() => <LandingPage/>} />
-      <Route path='/home' render={()=> <Home/>} /> 
-      <Route path='/createPatient' render={()=> <CreatePatient/>}/>
+      <Route exact path="/" render={() => <LandingPage />} />
+      <Route path="/home" render={() => <Home />} />
+      <Route path="/createPatient" render={() => <RegisterPatient />} />
     </div>
   );
 }
 
-// linea 9: del odontologo 
+// linea 9: del odontologo
 
 export default App;
