ad77e31a230baf73e370ec992b5a2da509680e28
EtienneLemoine <98330525+EtienneLemoine@users.noreply.github.com>
2022-05-24 13:57:06 -0300

    Cosas

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,22 +1,36 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-// import "./App.css";
+import "./App.css";
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./dentist/home";
+import RegisterPatient from "./dentist/home/navbar/register patient/";
+import Calendar from "./dentist/home/calendar";
+import Budget from "./dentist/home/budget";
+import PatientHome from "./patient/home";
+////LOGIN
 import SignUp from "./Components/SignUp/SignUp.jsx";
 import SignIn from "./Components/SignIn/SignIn.jsx";
 import PasswordReset from "./Components/PasswordReset/PasswordReset.jsx";
-import NewPassword from "./Components/NewPassword/NewPassword"; 
-
+import NewPassword from "./Components/NewPassword/NewPassword";
 
 function App() {
   return (
-    <BrowserRouter>
+    <div className="App">
       <Routes>
+        <Route path="/home" render={() => <Home />} />
+        <Route path="/createPatient" render={() => <RegisterPatient />} />
+        <Route path="/home/calendar" render={() => <Calendar />} />
+        <Route path="/home/budget" render={() => <Budget />} />
+        <Route path="/patient-home" render={() => <PatientHome />} />
+        {/* RUTAS LOGIN */}
         <Route path="/" element={<SignIn />} />
         <Route path="/SignUp" element={<SignUp />} />
         <Route path="/passwordReset" element={<PasswordReset />} />
-        <Route path="/newPassword" element={<NewPassword />} /> 
+        <Route path="/newPassword" element={<NewPassword />} />
       </Routes>
-    </BrowserRouter>
+    </div>
   );
 }
+
+// path Home: del odontologo
 
 export default App;
--- Client/src/index.js
+++ Client/src/index.js
@@ -1,19 +1,33 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import axios from "axios";
-import { Provider } from "react-redux";
-import store from "./redux/store/store";
-
-axios.defaults.baseURL = "http://localhost:3001"
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { dashboardTheme } from './common/theme/dashboardTheme';
+import { MuiPickersUtilsProvider } from '@material-ui/pickers';
+import DateFnsUtils from '@date-io/date-fns';
+import store from './redux/store.js';
 
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <ThemeProvider theme={dashboardTheme}>
+          <MuiPickersUtilsProvider utils={DateFnsUtils}>
+            <App />
+          </MuiPickersUtilsProvider>
+        </ThemeProvider>
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
