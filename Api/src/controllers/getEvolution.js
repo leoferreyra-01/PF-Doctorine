8c7cef1a78dd5f5423ea7588b58deef2dca8263b
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-05-26 14:30:10 -0300

    throw new Error x4

--- Api/src/controllers/getEvolution.js
+++ Api/src/controllers/getEvolution.js
@@ -1,19 +1,32 @@
-const { Evolution } = require('../db');
+const { Evolution, Patient } = require('../db');
 
 async function getEvolution() {
   const evo = await Evolution.findAll();
+
+  if (!evo.length) throw new Error(`No Evolutions found.`);
+
   return evo;
 }
 
 async function getEvolutionID(id) {
   const evo = await Evolution.findByPk(id);
+
+  if (!evo) throw new Error(`There is no Evolution with "EvolutionID=${id}".`);
+
   return evo;
 }
 
-async function getEvolutionPatient(patientID) {
-  const evoPatient = await Evolution.findAll({
-    where: { PatientID: patientID },
+async function getEvolutionPatient(PatientID) {
+  const findPatient = await Patient.findByPk(PatientID, {
+    include: [Evolution],
   });
+
+  if (!findPatient)
+    throw new Error(`There is no Patient with "PatientID=${PatientID}".`);
+
+  const evoPatient = findPatient.dataValues.Evolution;
+  if (!evoPatient.length) throw new Error(`No Evolutions found.`);
+
   return evoPatient;
 }
 
