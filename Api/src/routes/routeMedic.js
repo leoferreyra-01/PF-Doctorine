b461d25ba4cdbe576f97ad73afdc186bd04d3932
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-05-26 13:19:32 -0300

    throw new Error(`There is no Medic...

--- Api/src/routes/routeMedic.js
+++ Api/src/routes/routeMedic.js
@@ -74,8 +74,7 @@ router.delete('/:ID', async (req, res) => {
   const { ID } = req.params;
 
   try {
-    deleteMedic(ID);
-    res.status(200).send('Medic deleted.');
+    res.status(200).send(await deleteMedic(ID));
   } catch (error) {
     console.log(error);
     res.status(400).send(error.message);
