4aa166ff57c2cbc1ccf23b05b52af0ea7382f676
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-05-26 15:27:45 -0300

    Model/User validations

--- Api/src/models/User.js
+++ Api/src/models/User.js
@@ -14,47 +14,98 @@ module.exports = sequelize => {
     document: {
       type: DataTypes.INTEGER,
       alllownull: false,
+      unique: true,
+      validate: {
+        isInt: true,
+      },
     },
     name: {
       type: DataTypes.STRING(10),
       alllownull: false,
+      validate: {
+        notEmpty: true,
+        // is name or lastname or a string with accent and apostrophes
+        is: /[^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+/,
+      },
     },
     lastName: {
       type: DataTypes.STRING(20),
       alllownull: false,
+      validate: {
+        notEmpty: true,
+        // is name or lastname or a string with accent and apostrophes
+        is: /[^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+/,
+      },
     },
     birth: {
-      type: DataTypes.DATE,
+      type: DataTypes.DATEONLY,
       alllownull: false,
+      validate: {
+        // Date (yyyy-MM-dd)
+        is: /^(19[0-9]{2}|2[0-9]{3})-(0[1-9]|1[012])-([123]0|[012][1-9]|31)$/,
+      },
     },
     telephone: {
       type: DataTypes.STRING,
+      validate: {
+        // see https://regexr.com/39t6d for number formats.
+        is: /(\+?( |-|\.)?\d{1,2}( |-|\.)?)?(\(?\d{3}\)?|\d{3})( |-|\.)?(\d{3}( |-|\.)?\d{4})/g,
+      },
     },
     cellphone: {
       type: DataTypes.STRING,
+      validate: {},
     },
     street: {
       type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        // is name or lastname or a string with accent and apostrophes
+        is: /[^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+/,
+      },
     },
     number: {
       type: DataTypes.INTEGER,
+      validate: {
+        isInt: true,
+      },
     },
     city: {
       type: DataTypes.STRING,
+      validate: {
+        notEmpty: true,
+        // is name or lastname or a string with accent and apostrophes
+        is: /[^0-9\.\,\"\?\!\;\:\#\$\%\&\(\)\*\+\-\/\<\>\=\@\[\]\\\^\_\{\}\|\~]+/,
+      },
     },
     postalCode: {
       type: DataTypes.INTEGER,
+      validate: {
+        isInt: true,
+      },
     },
     email: {
       type: DataTypes.STRING,
       alllownull: false,
+      unique: true,
+      validate: {
+        isEmail: true,
+      },
     },
     password: {
       type: DataTypes.STRING,
       alllownull: false,
+      validate: {
+        notEmpty: true,
+        // - at least 8 characters, must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number. Can contain special characters
+        is: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm,
+      },
     },
     imageProfile: {
       type: DataTypes.STRING,
+      validate: {
+        isUrl: true,
+      },
     },
   });
 };
