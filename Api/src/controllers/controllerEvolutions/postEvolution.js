fe4838bfb3190f902bb32d07a3fca67a3e21fbf8
Leonardo Ferreyra <leo.ferreyra060@gmail.com>
2022-05-27 03:09:52 -0300

    Validation Evolution

--- Api/src/controllers/controllerEvolutions/postEvolution.js
+++ Api/src/controllers/controllerEvolutions/postEvolution.js
@@ -1,26 +1,44 @@
-const { Medic, Patient, Evolution } = require('../../db');
+'use strict';
+
+//|> SEQUELIZE
+
+const { Medic, Patient, Evolution, Teeth, Treatment } = require('../../db');
+
+//|> CONTROLLER
 
 async function postEvolution({
   date,
   observations,
+  PatientID,
+  MedicID,
   TreatmentID,
   toothID,
-  medic,
-  patient,
 }) {
+  let addMedics = await Medic.findByPk(MedicID);
+  let addPatients = await Patient.findByPk(PatientID);
+  let tooth = await Teeth.findByPk(toothID);
+  let treatment = await Treatment.findByPk(TreatmentID);
+
+  if (!addMedics) throw new Error(`There is no Medic with ID: ${MedicID}`);
+  if (!addPatients)
+    throw new Error(`There is no Patient with ID: ${PatientID}`);
+  if (!tooth)
+    throw new Error(`There is no Teeth registered with ID: ${toothID}`);
+  if (!treatment)
+    throw new Error(
+      `There is no Treatment registered with ID: ${TreatmentID}`
+    );
+
   let createEvolution = await Evolution.create({
     date,
     observations,
     TreatmentID,
     toothID,
   });
-  let addMedics = await Medic.findOne({
-    where: { ID: medic },
-  });
-  let addPatients = await Patient.findOne({ where: { ID: patient } });
   await createEvolution.setMedic(addMedics);
   await createEvolution.setPatient(addPatients);
-  return createEvolution;
+
+  return { success: 'Evolution created correctly', Evolution: createEvolution };
 }
 
 module.exports = { postEvolution };
