6826fe7734a1f41c3888593eab568ccb9ae27558
Leonardo Ferreyra <leo.ferreyra060@gmail.com>
2022-05-29 17:28:39 -0300

    deploy

--- Api/src/server.js
+++ Api/src/server.js
@@ -1,36 +1,33 @@
 const express = require('express');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
+const morgan = require('morgan');
 const routes = require('./routes/index.js');
 const cors = require('cors');
-require('./db.js');
+
+require('./db')
 
 const server = express();
-server.name = 'APINOS';
 
-// var corsOptions = {
-//   origin: 'https://pf-indefinido.vercel.app',
-//   optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-// }
+server.name = 'API';
 
 server.use(cors());
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
+server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Origin', '*'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
   next();
 });
 
-server.use(express.json());
 server.use('/', routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => {
-  // eslint-disable-line no-unused-vars
+server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
