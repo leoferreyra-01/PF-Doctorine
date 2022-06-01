28dd4774d0fcfbcd393e7864530a32bc78be58b1
Leonardo Ferreyra <leo.ferreyra060@gmail.com>
2022-06-01 13:05:38 -0300

    Components Actualizado <3

---
+++ client/src/Components/Loader/loader.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import s from './loader.module.css';
+export default function Loader({ setLoader }) {
+  {
+    setTimeout(() => {
+      setLoader(false);
+    }, 2000);
+  }
+  return (
+    <div className={s.container}>
+      <div className={s.spinner}>
+        <div className={s.inner}></div>
+      </div>
+    </div>
+  );
+}
