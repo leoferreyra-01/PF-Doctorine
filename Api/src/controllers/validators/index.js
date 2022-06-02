1f52c21f9aed23fa9779ed58b03ade4f9d33caa3
Alfonso, Montes de Oca <97349912+AlfonsoM0@users.noreply.github.com>
2022-06-02 01:56:47 -0300

    7) express-validations PACK

--- Api/src/controllers/validators/index.js
+++ Api/src/controllers/validators/index.js
@@ -1,15 +1,21 @@
 'use strict';
-const { validateInfoUser } = require('./User');
-const { validateInfoMedic } = require('./Medic');
+const { validateInfoUser, XvalidateInfoUser } = require('./User');
+const { validateInfoMedic, XvalidateInfoMedic } = require('./Medic');
 const { validateInfoPatient, XvalidateInfoPatient } = require('./Patient');
 const { validateModelID, XvalidateModelID } = require('./ModelID');
-const { validateTurnCollisions } = require('./TurnCollisions');
+const {
+  validateTurnCollisions,
+  XvalidateTurnCollisions,
+} = require('./TurnCollisions');
 
 const { XvalidateResults } = require('./XvalidateResults');
 
 const validate = {
   InfoUser: validateInfoUser,
+  xInfoUser: XvalidateInfoUser,
+
   InfoMedic: validateInfoMedic,
+  xInfoMedic: XvalidateInfoMedic,
 
   InfoPatient: validateInfoPatient,
   xInfoPatient: XvalidateInfoPatient,
@@ -18,6 +24,8 @@ const validate = {
   xModelID: XvalidateModelID,
 
   TurnCollisions: validateTurnCollisions,
+  xTurnCollisions: XvalidateTurnCollisions,
+
   xResults: XvalidateResults,
 };
 
