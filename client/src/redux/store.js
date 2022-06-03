45b2144c7224110d28e09d7e95d3fb081b2c027b
Leonardo Ferreyra <leo.ferreyra060@gmail.com>
2022-06-03 20:07:51 -0300

    ConfigureStore Updated

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,15 +1,20 @@
-import { createStore, compose, applyMiddleware } from 'redux';
+// import { createStore, compose, applyMiddleware } from 'redux';
 import rootReducer from '../redux/reducer';
-import thunk from 'redux-thunk';
+// import thunk from 'redux-thunk';
+import {configureStore} from '@reduxjs/toolkit';
 
-const composeEnhancers =
-  (typeof window !== 'undefined' &&
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
-  compose;
+// const composeEnhancers =
+//   (typeof window !== 'undefined' &&
+//     window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+//   compose;
 
-const store = createStore(
-  rootReducer,
-  composeEnhancers(applyMiddleware(thunk))
-);
+// const store = createStore(
+//   rootReducer,
+//   composeEnhancers(applyMiddleware(thunk))
+// );
+
+const store = configureStore({
+  reducer: rootReducer,
+})
 
 export default store;
