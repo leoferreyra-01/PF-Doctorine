9b004e4c7c777c5812fc755b96cc26621d290c99
Leonardo Ferreyra <leo.ferreyra060@gmail.com>
2022-06-03 20:37:43 -0300

    Precarga Index by Alfonso

--- Api/preload_db/index.js
+++ Api/preload_db/index.js
@@ -156,7 +156,6 @@ async function addUserPatient(n) {
   const newPatient = await Patient.create(infoPatient);
 
   newPatient.createUser(infoUser);
-  newPatient.createClinicalHistory(); // all default
 }
 
 async function addTeeths() {
