de5f70196a4bef45e3f9f17e115c966dd1ddb31b
Laura Villarroel <lauramar.villarroel@gmail.com>
2022-06-02 21:11:07 -0500

    actualizacion validacion ruta treatments

---
+++ Api/src/validators/validatorTreatments.js
@@ -0,0 +1,115 @@
+//|> SEQUELIZE
+const { Treatment, Clinic } = require('../db');
+//|> EXPRESS-VALIDATOR
+const { check } = require('express-validator');
+//|> VALIDATOR
+var validator = require('validator');
+const { validateResult } = require('../helpers/helpersClinic');
+
+const validateTreatments = [
+  //|> ID
+  check('ID', 'Must be a string.')
+    .custom(value => {
+      if (!value) {
+        throw new Error('ID is required.');
+      }
+      return true;
+    })
+    /*  .if(check('ID').exists())  */ // creo que no es necesario
+    /* .isString() */
+    .isNumeric() //|!| revisar
+    /* .isLength({ min: 4, max: 6 }) */
+    .custom(async (value, { req }) => {
+      //|!| aplica solo para post
+      if (req.method === 'POST' && value) {
+        const TreatmentDB = await Treatment.findByPk(value);
+
+        if (TreatmentDB)
+          throw new Error(
+            `ID ${value} already exists, please choose another ID!.`
+          );
+
+        return true;
+      }
+    })
+    .custom(async (value, { req }) => {
+      //|!| aplica solo para pUT Revisar
+      if (req.method === 'PUT' && value) {
+        const TreatmentDB = await Treatment.findByPk(value);
+
+        if (!TreatmentDB)
+          throw new Error(
+            `ID ${value} does not exist!, please choose another ID!.`
+          );
+
+        return true;
+      }
+    })
+    .custom(async (value, { req }) => {
+      //|!| aplica solo para DELETE Revisar value que llega por parametro
+      if (req.method === 'DELETE' && value) {
+        const TreatmentDB = await Treatment.findByPk(value);
+
+        if (!TreatmentDB)
+          throw new Error(
+            `ID ${value} does not exist!, please choose another ID!.`
+          );
+
+        return true;
+      }
+    }), //|> price
+  check('price', 'Must be a Float.')
+    .custom((value, { req }) => {
+      if (req.method === 'POST' && !value) {
+        throw new Error('price is required.');
+      }
+      return true;
+    })
+    .custom((value, { req }) => {
+      if (req.method === 'PUT' && !value) {
+        throw new Error('price is required.');
+      }
+      return true;
+    })
+    .if(check('price').exists())
+    .isFloat(),
+
+  //|> treatmentType
+  check('treatmentType', 'Must be a string.')
+    .custom((value, { req }) => {
+      if (req.method === 'POST' && !value) {
+        throw new Error('treatmentType is required.');
+      }
+      return true;
+    })
+    .if(check('treatmentType').exists())
+    .isAlphanumeric('en-US', { ignore: ' _-' }),
+  //|> description
+  check('description', 'Must be a string.')
+    .custom((value, { req }) => {
+      if (req.method === 'POST' && !value) {
+        throw new Error('description is required.');
+      }
+      return true;
+    })
+    .if(check('description').exists())
+    .isAlphanumeric('en-US', { ignore: ' _-.,' }),
+  //|> ClinicID
+  check('ClinicID')
+    .if(check('ClinicID').exists())
+    .custom(async value => {
+      //|!| aplica solo para POST
+      const clinicDB = await Clinic.findByPk(value);
+      // ID exist
+      if (!clinicDB) {
+        throw new Error(`ID ${value} does not exist!.`);
+      }
+
+      return true;
+    }),
+  (req, res, next) => {
+    validateResult(req, res, next);
+  },
+];
+
+module.exports = { validateTreatments };
