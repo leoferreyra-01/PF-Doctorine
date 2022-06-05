1555bf0328c30ff22ce9631e0d5bf843b18f6f04
Leonardo Ferreyra <leo.ferreyra060@gmail.com>
2022-06-05 14:24:25 -0300

    Update UpdatePatient.jsx

--- client/src/views/dentist/UpdatePatient/UpdatePatient.jsx
+++ client/src/views/dentist/UpdatePatient/UpdatePatient.jsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useState } from "react";
-import { Link, useNavigate, useParams } from "react-router-dom";
-import toast, { Toaster } from "react-hot-toast";
-import { useDispatch, useSelector } from "react-redux";
-import S from "./UpdatePatient.module.css";
-import axios from "axios";
+import React, { useState } from 'react';
+import { Link, useNavigate, useParams } from 'react-router-dom';
+import toast, { Toaster } from 'react-hot-toast';
+import S from './UpdatePatient.module.css';
+import { updatePatient } from '../../../redux/actions';
+import { useDispatch } from 'react-redux';
 
 export function validate(data) {
   let errors = {};
@@ -16,17 +16,16 @@ function addEvolution() {
   const navigate = useNavigate();
 
   const [data, setData] = useState({
-    name: "",
-    lastName: "",
-    document: "",
-    birth: "",
-    tepephone: "",
-    cellphone: "",
-    street: "",
-    number: "",
-    city: "",
-    postalCode: "",
-    medicalService: "",
+    name: '',
+    lastName: '',
+    birth: '',
+    telephone: '',
+    cellphone: '',
+    street: '',
+    number: '',
+    city: '',
+    postalCode: '',
+    medicalService: '',
   });
   const [errors, setErrors] = useState({});
 
@@ -42,9 +41,8 @@ function addEvolution() {
   const infoUser = {
     name: data.name,
     lastName: data.lastName,
-    document: parseInt(data.document),
     birth: data.birth,
-    tepephone: parseInt(data.tepephone),
+    telephone: parseInt(data.telephone),
     cellphone: parseInt(data.cellphone),
     street: data.street,
     number: parseInt(data.number),
@@ -54,149 +52,129 @@ function addEvolution() {
   const infoPatient = {
     medicalService: data.medicalService,
   };
-  const ID = parseInt(patientID);
 
   function handleSubmit() {
     console.log({ infoUser, infoPatient, patientID });
-    axios
-      .put(`/patients/${patientID}`, {
-        ID: ID,
-        infoUser: infoUser,
-        infoPatient: infoPatient,
-      })
-      .then((response) => {
-        toast.success(response.data.success);
-        navigate("/");
-      })
-      .catch(() => {
-        return toast.error("Este usuario ya ha sido creado.");
-      });
+    try {
+      dispatch(updatePatient(patientID, infoPatient, infoUser));
+      toast.success('Patient updated successfully');
+      navigate(`/home/${patientID}`);
+    } catch (err) {
+      toast.error("This patient couldn't be updated");
+    }
   }
 
   return (
     <>
-      <Toaster position="top-center" reverseOrder={false} />
+      <Toaster position='top-center' reverseOrder={false} />
       <div className={S.content}>
-        <form className={S.form}>
-          
-            <label className={S.label}>Frist Name</label>
-            <input
-              value={data.name}
-              placeholder="Name..."
-              type="text"
-              name="name"
-              onChange={handleChange}
-            />
-            {errors.observations && <p>{errors.observations}</p>}
+        <form className={S.form} onSubmit={handleSubmit}>
+          <label className={S.label}>Frist Name</label>
+          <input
+            value={data.name}
+            placeholder='Name...'
+            type='text'
+            name='name'
+            onChange={handleChange}
+          />
+          {errors.observations && <p>{errors.observations}</p>}
 
-            <label className={S.label}>Last Name</label>
-            <input
-              value={data.lastName}
-              placeholder="LastName..."
-              type="text"
-              name="lastName"
-              onChange={handleChange}
-            />
-            {errors.observations && <p>{errors.observations}</p>}
+          <label className={S.label}>Last Name</label>
+          <input
+            value={data.lastName}
+            placeholder='LastName...'
+            type='text'
+            name='lastName'
+            onChange={handleChange}
+          />
+          {errors.observations && <p>{errors.observations}</p>}
 
-            <label className={S.label}>Document</label>
-            <input
-              value={data.document}
-              placeholder="Document..."
-              type="text"
-              name="document"
-              onChange={handleChange}
-            />
-            {errors.observations && <p>{errors.observations}</p>}
+          <label className={S.label}>Birth</label>
+          <input
+            value={data.birth}
+            placeholder='Birth...'
+            type='date'
+            name='birth'
+            onChange={handleChange}
+          />
+          {errors.observations && <p>{errors.observations}</p>}
 
-            <label className={S.label}>Birth</label>
-            <input
-              value={data.birth}
-              placeholder="Birth..."
-              type="date"
-              name="birth"
-              onChange={handleChange}
-            />
-            {errors.observations && <p>{errors.observations}</p>}
+          <label className={S.label}>Telephone</label>
+          <input
+            value={data.telephone}
+            placeholder='Telephone...'
+            type='text'
+            name='telephone'
+            onChange={handleChange}
+          />
+          {errors.observations && <p>{errors.observations}</p>}
 
-            <label className={S.label}>Telephone</label>
-            <input
-              value={data.tepephone}
-              placeholder="Telephone..."
-              type="text"
-              name="telephone"
-              onChange={handleChange}
-            />
-            {errors.observations && <p>{errors.observations}</p>}
+          <label className={S.label}>Cellphone</label>
+          <input
+            value={data.cellphone}
+            placeholder='Cellphone...'
+            type='text'
+            name='cellphone'
+            onChange={handleChange}
+          />
+          {errors.observations && <p>{errors.observations}</p>}
 
-            <label className={S.label}>Cellphone</label>
-            <input
-              value={data.cellphone}
-              placeholder="Cellphone..."
-              type="text"
-              name="cellphone"
-              onChange={handleChange}
-            />
-            {errors.observations && <p>{errors.observations}</p>}
+          <label className={S.label}>Street</label>
+          <input
+            value={data.street}
+            placeholder='Street...'
+            type='text'
+            name='street'
+            onChange={handleChange}
+          />
+          {errors.observations && <p>{errors.observations}</p>}
 
-            <label className={S.label}>Street</label>
-            <input
-              value={data.street}
-              placeholder="Street..."
-              type="text"
-              name="street"
-              onChange={handleChange}
-            />
-            {errors.observations && <p>{errors.observations}</p>}
+          <label className={S.label}>Number</label>
+          <input
+            value={data.number}
+            placeholder='Number...'
+            type='text'
+            name='number'
+            onChange={handleChange}
+          />
+          {errors.observations && <p>{errors.observations}</p>}
 
-            <label className={S.label}>Number</label>
-            <input
-              value={data.number}
-              placeholder="Number..."
-              type="text"
-              name="number"
-              onChange={handleChange}
-            />
-            {errors.observations && <p>{errors.observations}</p>}
+          <label className={S.label}>City</label>
+          <input
+            value={data.city}
+            placeholder='City...'
+            type='text'
+            name='city'
+            onChange={handleChange}
+          />
+          {errors.observations && <p>{errors.observations}</p>}
 
-            <label className={S.label}>City</label>
-            <input
-              value={data.city}
-              placeholder="City..."
-              type="text"
-              name="city"
-              onChange={handleChange}
-            />
-            {errors.observations && <p>{errors.observations}</p>}
+          <label className={S.label}>Postal Code</label>
+          <input
+            value={data.postalCode}
+            placeholder='Postal Code...'
+            type='text'
+            name='postalCode'
+            onChange={handleChange}
+          />
+          {errors.observations && <p>{errors.observations}</p>}
 
-            <label className={S.label}>Postal Code</label>
-            <input
-              value={data.postalCode}
-              placeholder="Postal Code..."
-              type="text"
-              name="postalCode"
-              onChange={handleChange}
-            />
-            {errors.observations && <p>{errors.observations}</p>}
+          <label className={S.label}>Medical Service</label>
+          <input
+            value={data.medicalService}
+            placeholder='Medical Service...'
+            type='text'
+            name='medicalService'
+            onChange={handleChange}
+          />
+          {errors.observations && <p>{errors.observations}</p>}
 
-            <label className={S.label}>Medical Service</label>
-            <input
-              value={data.medicalService}
-              placeholder="Medical Service..."
-              type="text"
-              name="medicalService"
-              onChange={handleChange}
-            />
-            {errors.observations && <p>{errors.observations}</p>}
-
-            <button type="submit" className={S.btn} onClick={handleSubmit}>
-              Update Patient
-            </button>
+          <button type='submit' className={S.btn}>
+            Update Patient
+          </button>
         </form>
         <Link to={`/home/${patientID}`}>
-        <button className={S.btnBack}>
-              Back
-        </button>
+          <button className={S.btnBack}>Back</button>
         </Link>
       </div>
     </>
