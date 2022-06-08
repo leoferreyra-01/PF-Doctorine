c09660de655bac7d08f57f64dfa2394d9fe37be7
ErnestoAbril <abrilernesto@gmail.com>
2022-06-08 10:53:00 -0500

    actualizacion de pacientes

--- client/src/views/dentist/UpdatePatient/UpdatePatient.jsx
+++ client/src/views/dentist/UpdatePatient/UpdatePatient.jsx
@@ -71,7 +71,7 @@ export default function UpdatePatient() {
 
   function handleSubmit(e) {
     e.preventDefault();
-    console.log({ infoUser, infoPatient, patientID });
+    // console.log({ infoUser, infoPatient, patientID });
 
     try {
       if (fail) {
