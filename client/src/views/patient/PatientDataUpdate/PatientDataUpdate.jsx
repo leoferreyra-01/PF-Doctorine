7244d98dedcfebdafb4c4292f66bbd9d354c2bb6
Jimmy2414 <96069053+Jimmy2414@users.noreply.github.com>
2022-06-09 12:19:55 -0300

    Update PatientDataUpdate.jsx

--- client/src/views/patient/PatientDataUpdate/PatientDataUpdate.jsx
+++ client/src/views/patient/PatientDataUpdate/PatientDataUpdate.jsx
@@ -3,33 +3,15 @@ import { useSelector, useDispatch } from 'react-redux';
 import { useNavigate } from 'react-router-dom';
 import { updatePatient } from '../../../redux/actions';
 import './PatientDataUpdate.css';
-<<<<<<< HEAD
 import toast from 'react-hot-toast';
 import { getPatientDni2 } from '../../../redux/actions';
 import bk_validate from '../../../helpers/backend_validators';
 const PatientDataUpdate = () => {
-=======
-// import { useForm } from 'react-hook-form';
-// import { yupResolver } from '@hookform/resolvers/yup';
-// import { PatientSchema } from './PatientSchema';
-// import Container from '@material-ui/core/Container';
-// import TextField from '@material-ui/core/TextField';
-// import { Button } from '@material-ui/core';
-// import Input from '@mui/material/Input';
-// import Grid from '@material-ui/core/Grid';
-// import Avatar from '@mui/material/Avatar';
-import { getPatientDni2 } from '../../../redux/actions';
-import bk_validate from '../../../helpers/backend_validators';
-import toast from 'react-hot-toast';
-
-
-export default function PatientDataUpdate() {
->>>>>>> cc26f2cdfffa68b7fafc60744f4458bdf4552d5e
   const [errors, setErrors] = useState({});
   const navigate = useNavigate();
   const dispatch = useDispatch();
   const SearchedPatient = useSelector(state => state.searchedPatient);
-  const [validations, setValidations] = useState([false, null]);
+
   const uno = JSON.parse(window.localStorage.getItem('loggedToken'));
   const [validations, setValidations] = useState([false, null]);
 
@@ -45,7 +27,6 @@ export default function PatientDataUpdate() {
     telephone: SearchedPatient.telephone,
     cellphone: SearchedPatient.cellphone,
     email: SearchedPatient.email,
-<<<<<<< HEAD
   });
   async function validatePatient() {
     const [fail, err] = await bk_validate.Patient(
@@ -59,10 +40,6 @@ export default function PatientDataUpdate() {
     }
     console.log('VALIDATIONS Fun, 2 => ', validations);
   }
-=======
-    medicalService: '',
-  })
->>>>>>> cc26f2cdfffa68b7fafc60744f4458bdf4552d5e
 
   useEffect(() => {
     dispatch(getPatientDni2(uno.document));
@@ -70,25 +47,6 @@ export default function PatientDataUpdate() {
     // }
     validatePatient();
   }, [dispatch, user]);
-<<<<<<< HEAD
-=======
-
-  async function validatePatient() {
-    const [fail, err] = await bk_validate.Patient(
-      { infoUser, infoPatient },
-      patientID
-    );
-    if (fail) {
-      setValidations([true, err]);
-    } else {
-      setValidations([false, null]);
-    }
-    // console.log('VALIDATIONS Fun, 2 => ', validations);
-  }
-
-  let [fail, err] = validations;
-
->>>>>>> cc26f2cdfffa68b7fafc60744f4458bdf4552d5e
   // const {
   //   handleSubmit,
   //   register,
@@ -98,7 +56,6 @@ export default function PatientDataUpdate() {
   //   resolver: yupResolver(PatientSchema),
   // });
 
-<<<<<<< HEAD
   console.log(user);
   if (SearchedPatient.email === user.email) {
     var infoUser = {
@@ -127,45 +84,25 @@ export default function PatientDataUpdate() {
       userType: 'Patient',
       email: user.email,
     };
-=======
-  console.log(user)
-
-  const infoUser = {
-    name: user.name,
-    lastName: user.lastName,
-    birth: user.birth,
-    telephone: user.telephone + '',
-    cellphone: user.cellphone + '',
-    street: user.street,
-    number: (user.number),
-    city: user.city,
-    postalCode: (user.postalCode),
-    // userType: 'Patient'
->>>>>>> cc26f2cdfffa68b7fafc60744f4458bdf4552d5e
   }
 
   const infoPatient = {
     medicalService: user.medicalService,
   };
 
-<<<<<<< HEAD
   const patientID = SearchedPatient.Patient.ID;
-=======
-  const patientID = SearchedPatient.ID
->>>>>>> cc26f2cdfffa68b7fafc60744f4458bdf4552d5e
 
   const handleSubmit = e => {
     console.log(patientID, infoPatient, infoUser);
     // console.log({ infoUser, infoPatient, patientID });
-    console.log(fail)
-    console.log(err)
+    console.log(fail);
+    console.log(err);
     e.preventDefault();
 
     try {
       if (fail) {
         toast.error('Your form has errors, please check it out.');
       } else {
-<<<<<<< HEAD
         console.log(patientID, infoPatient, infoUser);
         dispatch(updatePatient(patientID, infoPatient, infoUser));
         setTimeout(() => {
@@ -176,14 +113,6 @@ export default function PatientDataUpdate() {
       }
     } catch (error) {
       console.log(errors);
-=======
-        dispatch(updatePatient(patientID, infoPatient, infoUser));
-        toast.success('Patient update successfully');
-        navigate(`/home`);
-      }
-    } catch (error) {
-      console.error(error);
->>>>>>> cc26f2cdfffa68b7fafc60744f4458bdf4552d5e
       toast.error('Something went wrong, please try again.');
     }
   };
@@ -198,7 +127,6 @@ export default function PatientDataUpdate() {
   };
 
   return (
-<<<<<<< HEAD
     <div className="container">
       <div className="container2">
         <form onSubmit={onSubmit}>
@@ -211,18 +139,6 @@ export default function PatientDataUpdate() {
                 value={user.document}
                 onChange={handleChange}
               ></input>
-=======
-    <div className='container'>
-      <div className='container2'>
-        <form onSubmit={handleSubmit}>
-          <div className='rowContainer'>
-            <div className='containerDivInput' style={{ width: '12vw' }}>
-              <div className='subtitle' >
-                Document
-              </div>
-              <input className='input' name='document' value={user.document} onChange={handleChange}>
-              </input>
->>>>>>> cc26f2cdfffa68b7fafc60744f4458bdf4552d5e
             </div>
             <div className="containerDivInput" style={{ width: '20vw' }}>
               <div className="subtitle">Name</div>
@@ -320,7 +236,6 @@ export default function PatientDataUpdate() {
                 onChange={handleChange}
               ></input>
             </div>
-<<<<<<< HEAD
           </div>{' '}
           <button type="submit" className="button">
             Update
@@ -331,23 +246,3 @@ export default function PatientDataUpdate() {
   );
 };
 export default PatientDataUpdate;
-=======
-          </div>
-          <button type="submit" className='button' >
-            Update
-          </button>
-        </form>
-      </div >
-    </div>
-  );
-}
-
-
-
-
-
-
-
-
-
->>>>>>> cc26f2cdfffa68b7fafc60744f4458bdf4552d5e
